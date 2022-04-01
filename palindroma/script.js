3a9ff79ebf3a571e6a932e5c21434063538badf1
FraAccaBi <francescahuda@gmail.com>
2022-04-01 16:26:41 +0200

    complete palindrome js

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,29 +1,36 @@
 /* Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
-let parola = prompt('inserisci una parola');
 
-let parolaArray = []
 
-for (let i = 0; i < parola.length; i++){
-    parolaArray[i] = parola.substring(i,(i + 1))    
+function isPalindrome(word) {
+    let parola = word
+    let parolaArray = []
+
+    for (let i = 0; i < parola.length; i++){
+        parolaArray[i] = parola.substring(i,(i + 1))    
+    }
+    console.log(parolaArray);
+    let parolaArrayInverso = []
+    parolaArrayInverso = parolaArray.reverse()
+    let parolaInversa = ''
+    for (let i = 0; i < parolaArrayInverso.length; i++){
+        parolaInversa += `${parolaArrayInverso[i]}`
+    }
+    console.log(parolaInversa);
+
+
+    if (parola === parolaInversa) {
+        return true
+    } else {
+        return false
+    }
 }
-console.log(parolaArray);
-let parolaArrayInverso = []
-parolaArrayInverso = parolaArray.reverse()
-let parolaInversa = ''
-for (let i = 0; i < parolaArrayInverso.length; i++){
-    parolaInversa += `${parolaArrayInverso[i]}`
-}
-console.log(parolaInversa);
 
+let palindrome = isPalindrome(prompt('inserisci parola'))
 
-if (parola === parolaInversa) {
-    console.log('la parola è palindroma');
+if (palindrome === true){
+    console.log('la parola è palidroma');
 } else {
-    console.log('la parola non è palindroma');
+    console.log('la parola non è palidroma');
 }
 
-
-function isPalindrome() {
-    
-}
